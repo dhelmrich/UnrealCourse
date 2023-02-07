670bf0b83c0b44328c29d5f8d8f1ff55a2c9e3d7
Dirk Helmrich <d.helmrich@fz-juelich.de>
2023-02-07 16:13:30 +0100

    Unreal Template

---
+++ Source/UnrealCourse/UnrealCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCourse, "UnrealCourse" );
