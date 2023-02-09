e026a6121b0f7b4105bb47be76870e5a309321e4
Jonathan Windgassen <j.windgassen@fz-juelich.de>
2023-02-09 17:24:06 +0100

    First Working Version

---
+++ Source/UnrealCourse/Public/GravitationalAttractorComponent.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GravitationalAttractorComponent.generated.h"
+
+
+class USphereComponent;
+
+
+UCLASS(BlueprintType, ClassGroup=Custom, meta=(BlueprintSpawnableComponent))
+class UNREALCOURSE_API UGravitationalAttractorComponent : public USceneComponent {
+    GENERATED_BODY()
+
+    UPROPERTY()
+    TSet<AActor*> ObjectsInSOI;
+
+public:
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+    USphereComponent* SphereOfInfluence;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    float Mass = 1;
+    
+    // Sets default values for this component's properties
+    UGravitationalAttractorComponent();
+
+    // Called when the game starts
+    virtual void BeginPlay() override;
+
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+                               FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+    UFUNCTION()
+    virtual void OnActorEnterSOI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                 const FHitResult& SweepResult);
+
+    UFUNCTION()
+    virtual void OnActorLeaveSOI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
