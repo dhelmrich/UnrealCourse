776496409e082501377c670f42772b41aad12c6c
Jonathan Windgassen <j.windgassen@fz-juelich.de>
2025-04-10 16:08:10 +0200

    Bump Version and set BuildSettings + Include Order to latest
    This should hopefully be compatible with the next 5.x versions, so we don't need to do this every time

--- Source/UnrealCourse/UnrealCourse.Build.cs
+++ Source/UnrealCourse/UnrealCourse.Build.cs
@@ -3,6 +3,9 @@ using UnrealBuildTool;
 public class UnrealCourse : ModuleRules {
 	public UnrealCourse(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		CppStandard = CppStandardVersion.Latest;
 
 		PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore" });
 	}
